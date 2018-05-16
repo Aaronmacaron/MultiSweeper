c53a931b2c5fed391e36d9bbef3bd14ae771dfee
mtte <kay.mattern@gibmit.ch>
2018-05-16 15:38:12 +0200

    Add client main class

---
+++ client/src/main/java/tk/aakado/multisweeper/client/App.java
@@ -0,0 +1,20 @@
+package tk.aakado.multisweeper.client;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * The client Application
+ */
+public class App extends Application {
+
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+
+}
