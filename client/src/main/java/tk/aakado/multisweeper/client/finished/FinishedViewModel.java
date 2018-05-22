676e4124ad6eed84f4ed5102eaa6fd8d774d6d3f
mtte <kay.mattern@gibmit.ch>
2018-05-22 21:33:43 +0200

    Add game finished view: fxml and class boilerplates

--- client/src/main/java/tk/aakado/multisweeper/client/finished/FinishedViewModel.java
+++ client/src/main/java/tk/aakado/multisweeper/client/finished/FinishedViewModel.java
@@ -1,6 +1,49 @@
 package tk.aakado.multisweeper.client.finished;
 
 import de.saxsys.mvvmfx.ViewModel;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.time.Duration;
 
 public class FinishedViewModel implements ViewModel {
+
+    private SimpleStringProperty victoryString = new SimpleStringProperty();
+    private SimpleIntegerProperty totalPlayers = new SimpleIntegerProperty();
+    private SimpleObjectProperty<Duration> totalTime = new SimpleObjectProperty<>();
+
+    public SimpleStringProperty victoryStringProperty() {
+        return victoryString;
+    }
+
+    public SimpleIntegerProperty totalPlayersProperty() {
+        return totalPlayers;
+    }
+
+    public SimpleObjectProperty<Duration> totalTimeProperty() {
+        return totalTime;
+    }
+
+    /**
+     * Starts a new game
+     */
+    public void startNewGame() {
+        // TODO: Implement
+    }
+
+    /**
+     * Reconfigure the game settings
+     */
+    public void reconfigure() {
+        // TODO: Implement
+    }
+
+    /**
+     * Disconnect from the server
+     */
+    public void disconnect() {
+        // TODO: Implement
+    }
+
 }
