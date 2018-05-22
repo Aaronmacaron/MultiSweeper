5fe0f80edc296ab0e743d02b6e6a8d289152b2c3
Aaron Ebnöther <aaron.ebnoether@gmail.com>
2018-05-22 19:32:06 +0200

    Add game view fxml and class boilerplates

--- client/src/main/java/tk/aakado/multisweeper/client/game/GameView.java
+++ client/src/main/java/tk/aakado/multisweeper/client/game/GameView.java
@@ -1,18 +1,54 @@
 package tk.aakado.multisweeper.client.game;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import de.saxsys.mvvmfx.InjectViewModel;
 import de.saxsys.mvvmfx.internal.viewloader.View;
+import javafx.beans.binding.Bindings;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.time.Duration;
+import java.util.ResourceBundle;
 
 public class GameView implements View<GameViewModel>, Initializable {
     @InjectViewModel
     private GameViewModel viewModel;
 
+    @FXML
+    private Label timeElapsed;
+    @FXML
+    private Label numberOfPlayers;
+    @FXML
+    private Label remainingMines;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        // Convert Duration to String and bind it to timeElapsed
+        timeElapsed.textProperty().bind(Bindings.createStringBinding(() -> {
+            Duration duration = viewModel.elapsedTimeProperty().get();
+            return String.format("%s:%s", duration.toMinutes(), duration.getSeconds());
+        }));
+
+        // Convert Int to String and bind it to numberOfPlayers
+        numberOfPlayers.textProperty().bind(Bindings.createStringBinding(() ->
+                Integer.toString(viewModel.numberOfPlayersProperty().get())));
+
+        // Convert Int to String and bind it to remainingMines
+        remainingMines.textProperty().bind(Bindings.createStringBinding(() ->
+                Integer.toString(viewModel.remainingMinesProperty().get())));
+
+    }
+
+    // Click handler for onRestart-button
+    public void onRestart(ActionEvent actionEvent) {
+        viewModel.restart();
+    }
+
+    // Click handler for onDisconnect-button
+    public void onDisconnect(ActionEvent actionEvent) {
+        viewModel.disconnect();
     }
 }
--- client/src/main/java/tk/aakado/multisweeper/client/game/GameViewModel.java
+++ client/src/main/java/tk/aakado/multisweeper/client/game/GameViewModel.java
@@ -1,6 +1,37 @@
 package tk.aakado.multisweeper.client.game;
 
 import de.saxsys.mvvmfx.ViewModel;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
+import java.time.Duration;
 
 public class GameViewModel implements ViewModel {
+
+    private SimpleObjectProperty<Duration> elapsedTime = new SimpleObjectProperty<>();
+    private SimpleIntegerProperty numberOfPlayers = new SimpleIntegerProperty();
+    private SimpleIntegerProperty remainingMines = new SimpleIntegerProperty();
+
+    public SimpleObjectProperty<Duration> elapsedTimeProperty() {
+        return elapsedTime;
+    }
+
+    public SimpleIntegerProperty numberOfPlayersProperty() {
+        return numberOfPlayers;
+    }
+
+    public SimpleIntegerProperty remainingMinesProperty() {
+        return remainingMines;
+    }
+
+    // Restarts the game
+    public void restart() {
+        // TODO: Implement
+    }
+
+    // Disconnects player from the game
+    public void disconnect() {
+        // TODO: Implement
+    }
+
 }
