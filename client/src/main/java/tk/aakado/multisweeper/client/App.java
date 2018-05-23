86e14013335f810b3d8431310c148db5f2d06df5
Dominik Strässle <dominik.straessle@gibmit.ch>
2018-05-23 09:25:27 +0200

    Created sample ViewTuples for executing views

--- client/src/main/java/tk/aakado/multisweeper/client/App.java
+++ client/src/main/java/tk/aakado/multisweeper/client/App.java
@@ -1,13 +1,23 @@
 package tk.aakado.multisweeper.client;
 
+import javax.security.auth.login.Configuration;
+
 import de.saxsys.mvvmfx.FluentViewLoader;
 import de.saxsys.mvvmfx.ViewTuple;
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import tk.aakado.multisweeper.client.authentication.AuthenticationView;
+import tk.aakado.multisweeper.client.authentication.AuthenticationViewModel;
+import tk.aakado.multisweeper.client.configuration.ConfigurationView;
+import tk.aakado.multisweeper.client.configuration.ConfigurationViewModel;
 import tk.aakado.multisweeper.client.connection.ConnectionView;
 import tk.aakado.multisweeper.client.connection.ConnectionViewModel;
+import tk.aakado.multisweeper.client.finished.FinishedView;
+import tk.aakado.multisweeper.client.finished.FinishedViewModel;
+import tk.aakado.multisweeper.client.game.GameView;
+import tk.aakado.multisweeper.client.game.GameViewModel;
 
 /**
  * The client Application
@@ -20,9 +30,15 @@ public class App extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        ViewTuple<ConnectionView, ConnectionViewModel> viewTuple = FluentViewLoader.fxmlView(ConnectionView.class).load();
+//        ViewTuple<ConnectionView, ConnectionViewModel> tuple = FluentViewLoader.fxmlView(ConnectionView.class).load();
+//        ViewTuple<AuthenticationView, AuthenticationViewModel> tuple = FluentViewLoader.fxmlView(AuthenticationView.class).load();
+//        ViewTuple<ConfigurationView, ConfigurationViewModel> tuple = FluentViewLoader.fxmlView(ConfigurationView.class).load();
+//        ViewTuple<GameView, GameViewModel> tuple = FluentViewLoader.fxmlView(GameView.class).load();
+        ViewTuple<FinishedView, FinishedViewModel> tuple = FluentViewLoader.fxmlView(FinishedView.class).load();
 
-        Parent root = viewTuple.getView();
+
+
+        Parent root = tuple.getView();
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
