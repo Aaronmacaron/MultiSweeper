d6458bb5e28287f802249f47c9066b124a2f98e9
mtte <kay.mattern@gibmit.ch>
2018-05-29 21:16:23 +0200

    Add PlayingField to UML diagram

---
+++ server/src/main/java/tk/aakado/multisweeper/server/PlayingField.java
@@ -0,0 +1,42 @@
+package tk.aakado.multisweeper.server;
+
+import java.util.List;
+
+public class PlayingField {
+
+    private final int width;
+    private final int height;
+    private final int numberOfMines;
+    private List<Field> fields;
+
+    public PlayingField(int width, int height, int numberOfMines) {
+        this.width = width;
+        this.height = height;
+        this.numberOfMines = numberOfMines;
+    }
+
+    public void generate() {
+
+    }
+
+    public void discoverField(FieldCords fieldCords) {
+
+    }
+
+    public void flagField(FieldCords fieldCords) {
+
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getNumberOfMines() {
+        return numberOfMines;
+    }
+
+}
