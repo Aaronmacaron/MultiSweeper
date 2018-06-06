9989e68a6d5b894f9cf9c7abe9e73ee4d330f37a
Aaron Ebnöther <aaron.ebnoether@gmail.com>
2018-06-06 10:15:41 +0200

    Implement Transmitter without any actual methods

--- client/src/main/java/tk/aakado/multisweeper/client/connection/Transmitter.java
+++ client/src/main/java/tk/aakado/multisweeper/client/connection/Transmitter.java
@@ -1,7 +1,18 @@
 package tk.aakado.multisweeper.client.connection;
 
+/**
+ * The Transmitter contains all IO/Server/Connector related methods that the Client potentially could execute.
+ */
 public class Transmitter {
 
-    private ClientConnector clientConnector;
+    final private ClientConnector clientConnector;
+
+    /**
+     * Constructor
+     * @param clientConnector The Connector that the Transmitter transmits through.
+     */
+    public Transmitter(ClientConnector clientConnector) {
+        this.clientConnector = clientConnector;
+    }
 
 }
