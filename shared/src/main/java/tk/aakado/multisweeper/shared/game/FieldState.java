1a0f75c920a33a1ec742432a034af193d31957fc
Dominik Strässle <dominik.straessle@gibmit.ch>
2018-06-13 09:42:25 +0200

    Add FieldState

---
+++ shared/src/main/java/tk/aakado/multisweeper/shared/game/FieldState.java
@@ -0,0 +1,5 @@
+package tk.aakado.multisweeper.shared.game;
+
+public enum FieldState {
+    FIELD_0, FIELD_1, FIELD_2, FIELD_3, FIELD_4, FIELD_5, FIELD_6, FIELD_7, FIELD_8, MINE, FLAG, FALSE_FLAGGED_MINE, UNDEFINED;
+}
