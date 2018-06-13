077aa15309ff9ad7c0467536fa7c33d5bd162362
mtte <kay.mattern@gibmit.ch>
2018-06-13 13:56:03 +0200

    Improve FieldState

--- shared/src/main/java/tk/aakado/multisweeper/shared/game/FieldDTO.java
+++ shared/src/main/java/tk/aakado/multisweeper/shared/game/FieldDTO.java
@@ -1,5 +1,7 @@
 package tk.aakado.multisweeper.shared.game;
 
+import java.util.Optional;
+
 /**
  * This data transfer object is used to transfer information about a field.
  */
@@ -8,11 +10,21 @@ public class FieldDTO {
     private final int x;
     private final int y;
     private final FieldState state;
+    private final Optional<Integer> value;
 
     public FieldDTO(int x, int y, FieldState state) {
+        this(x, y, state, Optional.empty());
+    }
+
+    public FieldDTO(int x, int y, FieldState state, int value) {
+        this(x, y, state, Optional.of(value));
+    }
+
+    private FieldDTO(int x, int y, FieldState state, Optional<Integer> value) {
         this.x = x;
         this.y = y;
         this.state = state;
+        this.value = value;
     }
 
     public int getX() {
@@ -25,5 +37,9 @@ public class FieldDTO {
 
     public FieldState getState() {
         return state;
+    }
+
+    public Optional<Integer> getValue() {
+        return value;
     }
 }
