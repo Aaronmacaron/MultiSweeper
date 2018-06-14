d7b72b965e00360cd61c1fd1cca2ed9c173f0816
Dominik Strässle <dominik.straessle@gibmit.ch>
2018-06-14 21:44:02 +0200

    Add javadoc

--- shared/src/main/java/tk/aakado/multisweeper/shared/game/FieldState.java
+++ shared/src/main/java/tk/aakado/multisweeper/shared/game/FieldState.java
@@ -1,5 +1,10 @@
 package tk.aakado.multisweeper.shared.game;
 
+/**
+ * All possible States of a field
+ *
+ * @author Dominik Strässle
+ */
 public enum FieldState {
     FIELD_0, FIELD_1, FIELD_2, FIELD_3, FIELD_4, FIELD_5, FIELD_6, FIELD_7, FIELD_8, MINE, FLAG, FALSE_FLAGGED_MINE, UNDEFINED;
 }
