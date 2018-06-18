ee94b06e5025fa47bc145230c78c7a93ae83f0bb
Dominik Strässle <dominik.straessle@gibmit.ch>
2018-06-18 11:10:30 +0200

    Implement and test overridden methods of the game notificator

--- client/src/main/java/tk/aakado/multisweeper/client/views/game/GameViewModel.java
+++ client/src/main/java/tk/aakado/multisweeper/client/views/game/GameViewModel.java
@@ -4,15 +4,25 @@ import java.time.Duration;
 
 import de.saxsys.mvvmfx.ViewModel;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ListProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import tk.aakado.multisweeper.client.views.game.model.Field;
+import tk.aakado.multisweeper.shared.Logger;
+import tk.aakado.multisweeper.shared.game.FieldState;
 
 public class GameViewModel implements ViewModel, GameNotificator {
 
     private ObjectProperty<Duration> elapsedTime = new SimpleObjectProperty<>(Duration.ZERO);//TODO to prevent NUllPointerException Duration.ZERO is set
     private IntegerProperty numberOfPlayers = new SimpleIntegerProperty();
     private IntegerProperty remainingMines = new SimpleIntegerProperty();
+    private ListProperty<Field> fields = new SimpleListProperty<>(FXCollections.emptyObservableList());
+    private IntegerProperty fieldWidth = new SimpleIntegerProperty();
+    private IntegerProperty fieldHeight = new SimpleIntegerProperty();
 
     @Override
     public void playerDisconnected(String player, boolean isNewAdmin) {
@@ -25,13 +35,41 @@ public class GameViewModel implements ViewModel, GameNotificator {
     }
 
     @Override
-    public void restart() {
-        // TODO: Implement
+    public void restart(int x, int y) {
+        // set the size of the field
+        this.fieldWidth.setValue(x);
+        this.fieldHeight.setValue(y);
+
+        // create a new empty ObservableArrayList
+        ObservableList<Field> newFields = FXCollections.observableArrayList();
+
+        // Create all required Fields and add them to the fields list
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                newFields.add(new Field(i, j, FieldState.UNDEFINED, 0));
+            }
+        }
+
+        // set the new fields as new value to the existing ListProperty
+        fields.setValue(newFields);
     }
 
     @Override
     public void updateField(int[] coords, String newState) {
-        //TODO: Implement
+
+        Field field = fields.get().stream()
+                .filter(f -> f.getX() == coords[0] && f.getY() == coords[1])
+                .findFirst()
+                .orElseThrow(() -> {
+                    Logger.get(this).error("A field with the coordinates %s:%s does not exist", coords[0], coords[1]);
+                    return new IllegalArgumentException("A field with the coordinates " + coords[0] + ":" + coords[1] + " does not exist");
+                });
+
+        try {
+            field.setFieldState(FieldState.valueOf(newState));
+        } catch (IllegalArgumentException ex) {
+            Logger.get(this).error("The field state %s does not exists", newState);
+        }
     }
 
     @Override
@@ -46,6 +84,22 @@ public class GameViewModel implements ViewModel, GameNotificator {
         // TODO: Implement
     }
 
+    /**
+     * Handles a click on a field
+     * Send the click to the server over the transmitter
+     *
+     * @param x x-coordinate of the field
+     * @param y y-coordinate of the field
+     */
+    public void click(int x, int y) {
+        fields.stream()
+                .filter(field -> field.getX() == x && field.getY() == y)
+                .findAny()
+                .get()
+                .setFieldState(FieldState.FLAG);
+        //TODO: Implement
+    }
+
     public ObjectProperty<Duration> elapsedTimeProperty() {
         return elapsedTime;
     }
@@ -58,4 +112,27 @@ public class GameViewModel implements ViewModel, GameNotificator {
         return remainingMines;
     }
 
+    public ObservableList<Field> getFields() {
+        return fields.get();
+    }
+
+    public ListProperty<Field> fieldsProperty() {
+        return fields;
+    }
+
+    public int getFieldWidth() {
+        return fieldWidth.get();
+    }
+
+    public IntegerProperty fieldWidthProperty() {
+        return fieldWidth;
+    }
+
+    public int getFieldHeight() {
+        return fieldHeight.get();
+    }
+
+    public IntegerProperty fieldHeightProperty() {
+        return fieldHeight;
+    }
 }
---
+++ client/src/test/java/tk/aakado/multisweeper/client/views/game/GameViewModelTest.java
@@ -0,0 +1,74 @@
+package tk.aakado.multisweeper.client.views.game;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import tk.aakado.multisweeper.client.views.game.model.Field;
+import tk.aakado.multisweeper.shared.game.FieldState;
+
+import static org.junit.Assert.assertEquals;
+
+public class GameViewModelTest {
+
+    private GameViewModel viewModel;
+
+    @Before
+    public void setUp() throws Exception {
+        viewModel = new GameViewModel();
+    }
+
+    @Test
+    public void restart() {
+        int x = 4;
+        int y = 4;
+        List<Field> exceptedFields = new ArrayList<>();
+
+        // create new fields
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                exceptedFields.add(new Field(i, j, FieldState.UNDEFINED, 0));
+            }
+        }
+
+        // let the viewmodel create new fields
+        viewModel.restart(x, y);
+        List<Field> actualFields = viewModel.getFields();
+
+        // check all fields
+        for (int i = 0; i < actualFields.size(); i++) {
+            assertEquals(exceptedFields.get(i), actualFields.get(i));
+        }
+
+    }
+
+    @Test
+    public void updateField() {
+        // create and add a field to the list
+        ObservableList<Field> fieldList = FXCollections.observableArrayList();
+        int[] coords = {0, 0};
+        String newState = "MINE";
+        fieldList.add(new Field(coords[0], coords[1], FieldState.UNDEFINED, 0));
+
+        // set the list as value
+        this.viewModel.fieldsProperty().setValue(fieldList);
+
+        // change the state of the field
+        this.viewModel.updateField(coords, newState);
+
+        // check if the new field state is set
+        assertEquals(FieldState.valueOf(newState), this.viewModel.getFields().get(0).getFieldState());
+
+        // try to change the state to an invalid state
+        this.viewModel.updateField(coords, "NOTPOSSIBLE");
+
+        // check if the state changed (should not)
+        assertEquals(FieldState.valueOf(newState), this.viewModel.getFields().get(0).getFieldState());
+
+
+    }
+}
