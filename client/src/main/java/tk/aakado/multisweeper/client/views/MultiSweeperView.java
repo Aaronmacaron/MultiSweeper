d52fe21710835831a00c29a5a28eca636e798391
mtte <kay.mattern@gibmit.ch>
2018-06-23 22:23:12 +0200

    Do not use the view for multiple scenes.
    The Scene gets now cached in the ms-view as a root (view) can only be used once for a scene.

--- client/src/main/java/tk/aakado/multisweeper/client/views/MultiSweeperView.java
+++ client/src/main/java/tk/aakado/multisweeper/client/views/MultiSweeperView.java
@@ -5,12 +5,14 @@ import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.ViewModel;
 import de.saxsys.mvvmfx.ViewTuple;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import tk.aakado.multisweeper.shared.Logger;
 
 /**
  * This class carries the information of a view.
- * It has the same purpose as the {@link ViewTuple} but it extends it with the notificator that corresponds to the
- * view model of this view. The notificator <strong>has</strong> to be implemented by the view model.
+ * It has the same purpose as the {@link ViewTuple} but it extends it with a scene containing the view
+ * and the notificator that corresponds to the view model of this view. The notificator <strong>has</strong>
+ * to be implemented by the view model.
  * @param <ViewType>        The generic type of the view
  * @param <ViewModelType>   The generic type of the view model
  * @param <ViewNotificator> The generic type of the notificator
@@ -18,12 +20,14 @@ import tk.aakado.multisweeper.shared.Logger;
 public class MultiSweeperView<ViewType extends FxmlView<? extends ViewModelType>, ViewModelType extends ViewModel, ViewNotificator extends Notificator> {
 
     private final ViewTuple<? extends ViewType, ViewModelType> tuple;
+    private final Scene scene;
 
     /**
      * @param viewType The view that should be loaded.
      */
     public MultiSweeperView(Class<? extends ViewType> viewType) {
         this.tuple = FluentViewLoader.fxmlView(viewType).load();
+        this.scene = new Scene(this.tuple.getView());
     }
 
     /**
@@ -64,6 +68,14 @@ public class MultiSweeperView<ViewType extends FxmlView<? extends ViewModelType>
             throw e;
         }
         return notificator;
+    }
+
+    /**
+     * Get the scene of this multi sweeper view with the View as root.
+     * @return The scene of this view.
+     */
+    public Scene getScene() {
+        return this.scene;
     }
 
 }
