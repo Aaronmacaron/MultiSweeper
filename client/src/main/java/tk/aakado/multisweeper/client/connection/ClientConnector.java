1e4df2ace9729f9dfb723fd730dd4b29007850cc
mtte <kay.mattern@gibmit.ch>
2018-06-24 22:17:36 +0200

    Correct logging

--- client/src/main/java/tk/aakado/multisweeper/client/connection/ClientConnector.java
+++ client/src/main/java/tk/aakado/multisweeper/client/connection/ClientConnector.java
@@ -125,7 +125,7 @@ public class ClientConnector extends AbstractConnector {
             method.invoke(method.getDeclaringClass().newInstance(), message);
         } catch (IllegalAccessException | InvocationTargetException e) {
             // Do nothing if method hasn't got the right parameters.
-            Logger.get(this).warn("Could not invoke method {} because it has the wrong parameters.", e);
+            Logger.get(this).warn("Could not invoke method {} because it has the wrong parameters.", method.getName(), e);
         } catch (InstantiationException e) {
             Logger.get(this).error("Could not instantiate action handler: " + method.getDeclaringClass().getSimpleName(), e);
         }
