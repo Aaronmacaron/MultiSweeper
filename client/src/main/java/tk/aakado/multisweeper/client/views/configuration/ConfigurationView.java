e6737114bf4cbaa34b52acdb83f458637385746d
mtte <kay.mattern@gibmit.ch>
2018-07-15 13:27:56 +0200

    Hide the whole configuration panel when the client is not an admin

--- client/src/main/java/tk/aakado/multisweeper/client/views/configuration/ConfigurationView.java
+++ client/src/main/java/tk/aakado/multisweeper/client/views/configuration/ConfigurationView.java
@@ -1,44 +1,39 @@
 package tk.aakado.multisweeper.client.views.configuration;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
 import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.Slider;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
 import javafx.util.converter.NumberStringConverter;
 import tk.aakado.multisweeper.client.Client;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class ConfigurationView implements FxmlView<ConfigurationViewModel>, Initializable {
     @InjectViewModel
     private ConfigurationViewModel viewModel;
 
     @FXML
+    private Pane mainContainer;
+    @FXML
+    private Pane configurationContainer;
+    @FXML
     private Slider mineDensitySlider;
-
     @FXML
     private TextField fieldWidthField;
-
     @FXML
     private TextField fieldHeightField;
-
     @FXML
     private PasswordField passwordField;
-
     @FXML
     private ListView<String> playersList;
-
     @FXML
     private Button startButton;
-
     @FXML
     private Button saveButton;
 
@@ -59,15 +54,28 @@ public class ConfigurationView implements FxmlView<ConfigurationViewModel>, Init
         // Bind playerslist unidirectional
         playersList.itemsProperty().bind(viewModel.playersProperty());
 
-        // Make all Configuration Properties not editable when Player isn't an admin
-        passwordField.editableProperty().bind(Client.getInstance().getGameProperties().adminProperty());
-        fieldHeightField.editableProperty().bind(Client.getInstance().getGameProperties().adminProperty());
-        fieldWidthField.editableProperty().bind(Client.getInstance().getGameProperties().adminProperty());
+        // Show / hide configuration panel
+        Client.getInstance().getGameProperties().adminProperty().addListener((observable, oldValue, isAdmin) -> this.toggleConfigurationPanel(isAdmin));
+        toggleConfigurationPanel(Client.getInstance().getGameProperties().isAdmin());
+
         // Make all Configuration Buttons disabled when Player isn't an admin
         mineDensitySlider.disableProperty().bind(Client.getInstance().getGameProperties().adminProperty().not());
         startButton.disableProperty().bind(Client.getInstance().getGameProperties().adminProperty().not());
         saveButton.disableProperty().bind(Client.getInstance().getGameProperties().adminProperty().not());
 
+    }
+
+    /**
+     * Shows/hides the configuration panel whether the client is an admin or not.
+     * @param isAdmin If the client is admin.
+     */
+    private void toggleConfigurationPanel(boolean isAdmin) {
+        boolean isCurrentlyActive = this.mainContainer.getChildren().contains(this.configurationContainer);
+        if (isAdmin && !isCurrentlyActive) {
+            this.mainContainer.getChildren().add(0, this.configurationContainer);
+        } else if (!isAdmin && isCurrentlyActive) {
+            this.mainContainer.getChildren().remove(this.configurationContainer);
+        }
     }
 
     /**
